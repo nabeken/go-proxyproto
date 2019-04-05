9329e03843dabef29eaf489644929ccecc3d3751
Tanabe Ken-ichi <nabeken@tknetworks.org>
2019-04-05 13:56:58 +0900

    make it the table test

--- protocol_test.go
+++ protocol_test.go
@@ -4,81 +4,144 @@ import (
 	"testing"
 )
 
-func TestTCPoverIPv4(t *testing.T) {
-	b := byte(TCPv4)
-	if !AddressFamilyAndProtocol(b).IsIPv4() {
-		t.Fail()
-	}
-	if !AddressFamilyAndProtocol(b).IsStream() {
-		t.Fail()
+func TestAddressFamilyAndProtocols(t *testing.T) {
+	for _, tt := range []struct {
+		B      AddressFamilyAndProtocol
+		TrueF  []func(AddressFamilyAndProtocol) bool
+		FalseF []func(AddressFamilyAndProtocol) bool
+	}{
+		{
+			B: UNSPEC,
+			TrueF: []func(AddressFamilyAndProtocol) bool{
+				AddressFamilyAndProtocol.IsUnspec,
+			},
+			FalseF: []func(AddressFamilyAndProtocol) bool{
+				AddressFamilyAndProtocol.IsIPv4,
+				AddressFamilyAndProtocol.IsStream,
+				AddressFamilyAndProtocol.IsIPv6,
+				AddressFamilyAndProtocol.IsDatagram,
+			},
+		},
+		{
+			B: TCPv4,
+			TrueF: []func(AddressFamilyAndProtocol) bool{
+				AddressFamilyAndProtocol.IsIPv4,
+				AddressFamilyAndProtocol.IsStream,
+			},
+			FalseF: []func(AddressFamilyAndProtocol) bool{
+				AddressFamilyAndProtocol.IsIPv6,
+				AddressFamilyAndProtocol.IsDatagram,
+				AddressFamilyAndProtocol.IsUnspec,
+			},
+		},
+		{
+			B: UDPv4,
+			TrueF: []func(AddressFamilyAndProtocol) bool{
+				AddressFamilyAndProtocol.IsIPv4,
+				AddressFamilyAndProtocol.IsDatagram,
+			},
+			FalseF: []func(AddressFamilyAndProtocol) bool{
+				AddressFamilyAndProtocol.IsIPv6,
+				AddressFamilyAndProtocol.IsStream,
+				AddressFamilyAndProtocol.IsUnspec,
+			},
+		},
+		{
+			B: TCPv6,
+			TrueF: []func(AddressFamilyAndProtocol) bool{
+				AddressFamilyAndProtocol.IsIPv6,
+				AddressFamilyAndProtocol.IsStream,
+			},
+			FalseF: []func(AddressFamilyAndProtocol) bool{
+				AddressFamilyAndProtocol.IsIPv4,
+				AddressFamilyAndProtocol.IsDatagram,
+				AddressFamilyAndProtocol.IsUnspec,
+			},
+		},
+		{
+			B: UDPv6,
+			TrueF: []func(AddressFamilyAndProtocol) bool{
+				AddressFamilyAndProtocol.IsIPv6,
+				AddressFamilyAndProtocol.IsDatagram,
+			},
+			FalseF: []func(AddressFamilyAndProtocol) bool{
+				AddressFamilyAndProtocol.IsIPv4,
+				AddressFamilyAndProtocol.IsStream,
+				AddressFamilyAndProtocol.IsUnspec,
+			},
+		},
+	} {
+		t.Run(string(tt.B), func(t *testing.T) {
+			for _, f := range tt.TrueF {
+				t.Run("TrueF", func(t *testing.T) {
+					if !f(tt.B) {
+						t.Error("must be true")
+					}
+				})
+			}
+			for _, f := range tt.FalseF {
+				t.Run("FalseF", func(t *testing.T) {
+					if f(tt.B) {
+						t.Error("must be false")
+					}
+				})
+			}
+		})
 	}
 }
 
-func TestTCPoverIPv6(t *testing.T) {
-	b := byte(TCPv6)
-	if !AddressFamilyAndProtocol(b).IsIPv6() {
-		t.Fail()
-	}
-	if !AddressFamilyAndProtocol(b).IsStream() {
-		t.Fail()
-	}
-}
-
-func TestUDPoverIPv4(t *testing.T) {
-	b := byte(UDPv4)
-	if !AddressFamilyAndProtocol(b).IsIPv4() {
-		t.Fail()
-	}
-	if !AddressFamilyAndProtocol(b).IsDatagram() {
-		t.Fail()
-	}
-}
-
-func TestUDPoverIPv6(t *testing.T) {
-	b := byte(UDPv6)
-	if !AddressFamilyAndProtocol(b).IsIPv6() {
-		t.Fail()
-	}
-	if !AddressFamilyAndProtocol(b).IsDatagram() {
-		t.Fail()
-	}
-}
-
-func TestInvalidAddressFamilyAndProtocol(t *testing.T) {
-	b := byte(UNSPEC)
-	if !AddressFamilyAndProtocol(b).IsUnspec() {
-		t.Fail()
-	}
-}
-
-func TestLocal(t *testing.T) {
-	b := byte(LOCAL)
-	if ProtocolVersionAndCommand(b).IsUnspec() {
-		t.Fail()
-	}
-	if !ProtocolVersionAndCommand(b).IsLocal() {
-		t.Fail()
-	}
-	if ProtocolVersionAndCommand(b).IsProxy() {
-		t.Fail()
-	}
-}
-
-func TestProxy(t *testing.T) {
-	b := byte(PROXY)
-	if ProtocolVersionAndCommand(b).IsUnspec() {
-		t.Fail()
-	}
-	if ProtocolVersionAndCommand(b).IsLocal() {
-		t.Fail()
-	}
-	if !ProtocolVersionAndCommand(b).IsProxy() {
-		t.Fail()
-	}
-}
-
-func TestInvalidProtocolVersion(t *testing.T) {
-	if !ProtocolVersionAndCommand(0x00).IsUnspec() {
-		t.Fail()
+func TestProtocolVersionAndCommand(t *testing.T) {
+	for _, tt := range []struct {
+		B      ProtocolVersionAndCommand
+		TrueF  []func(ProtocolVersionAndCommand) bool
+		FalseF []func(ProtocolVersionAndCommand) bool
+	}{
+		{
+			B: LOCAL,
+			TrueF: []func(ProtocolVersionAndCommand) bool{
+				ProtocolVersionAndCommand.IsLocal,
+			},
+			FalseF: []func(ProtocolVersionAndCommand) bool{
+				ProtocolVersionAndCommand.IsProxy,
+				ProtocolVersionAndCommand.IsUnspec,
+			},
+		},
+		{
+			B: PROXY,
+			TrueF: []func(ProtocolVersionAndCommand) bool{
+				ProtocolVersionAndCommand.IsProxy,
+			},
+			FalseF: []func(ProtocolVersionAndCommand) bool{
+				ProtocolVersionAndCommand.IsLocal,
+				ProtocolVersionAndCommand.IsUnspec,
+			},
+		},
+		{
+			B: 0x00,
+			TrueF: []func(ProtocolVersionAndCommand) bool{
+				ProtocolVersionAndCommand.IsUnspec,
+			},
+			FalseF: []func(ProtocolVersionAndCommand) bool{
+				ProtocolVersionAndCommand.IsProxy,
+				ProtocolVersionAndCommand.IsLocal,
+			},
+		},
+	} {
+		t.Run(string(tt.B), func(t *testing.T) {
+			for _, f := range tt.TrueF {
+				t.Run("TrueF", func(t *testing.T) {
+					if !f(tt.B) {
+						t.Error("must be true")
+					}
+				})
+			}
+			for _, f := range tt.FalseF {
+				t.Run("FalseF", func(t *testing.T) {
+					if f(tt.B) {
+						t.Error("must be false")
+					}
+				})
+			}
+		})
 	}
 }
